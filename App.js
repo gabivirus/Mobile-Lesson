7022f7c8695008ceec9324985411d117e3708b63
Gabriel Ribeiro <118030785+gabivirus@users.noreply.github.com>
2023-03-17 12:12:53 -0300

    Finished

--- App.js
+++ App.js
@@ -1,19 +1,13 @@
 import React, { useState, useEffect } from 'react';
-
 import { Text, View, StyleSheet, FlatList, Image } from 'react-native';
 import AppLoading from 'expo-app-loading';
-import {
-  useFonts,
-  JetBrainsMono_800ExtraBold,
-  Inter_400Regular,
-  Poppins_400Regular_Italic
-  } from '@expo-google-fonts/dev';
+import { useFonts, JetBrainsMono_100Thin, CormorantGaramond_600SemiBold, DancingScript_400Regular } from '@expo-google-fonts/dev';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
-    JetBrainsMono_800ExtraBold,
-    Inter_400Regular,
-    Poppins_400Regular_Italic
+    JetBrainsMono_100Thin,
+    CormorantGaramond_600SemiBold,
+    DancingScript_400Regular
   });
 
   if (!fontsLoaded) {
@@ -23,14 +17,17 @@ export default function App() {
       <View style={styles.container}>
         <Text style={styles.titulo}>Melhores ferramentas de estilzação</Text>
         <FlatList
-          data={arrayGear}
+          data={arraygear}
           renderItem={({ item }) =>
-            <View>
+            <View style={styles.secondView}>
               <Text style={styles.second}>
-                {item.Gear} - {item.ano}
+                {item.gear} - {item.ano}
               </Text>
-              <Image src={item.img}>
+              <Image source={item.img} style={styles.imagem}>
               </Image>
+              <Text style={styles.text}>
+                {item.func}
+              </Text>
             </View>
           }
         />
@@ -42,36 +39,63 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#fff',
     padding: 8,
+    alignItems: 'center',
+    backgroundColor: '#353d28',
   },
   titulo: {
     marginVertical: 20,
     fontSize: 40,
-    color: '#6940cf',
-    fontFamily: 'JetBrainsMono_800ExtraBold',
+    color: '#aec571',
+    fontFamily: 'JetBrainsMono_100Thin',
     fontWeight: 'bolder',
   },
+  secondView: {
+    display: 'flex',
+    alignItems: 'center',
+    marginVertical: 30,
+    padding: 20,
+    backgroundColor: '#3d472d',
+    color: '#b6bab1'
+  },
+  second:{
+    width: '100%',
+    fontSize: 30,
+    fontFamily: 'DancingScript_400Regular',
+    color: '#ffde7d',
+    borderBottomWidth: 2,
+    borderColor: '#ffde7d'
+  },
+  imagem: {
+    width: 320,
+    height: 180,
+    resizeImage: 'center',
+    marginVertical: 20,
+  },
+  text: {
+    fontFamily: 'CormorantGaramond_600SemiBold',
+    fontSize: 20,
+    color: '#ddd'
+  }
 });
 
-const arrayGear = [
+const arraygear = [
   {
-    Gear: 'Sass',
+    gear: 'Sass',
     ano: '2006',
     func: 'Sass é uma linguagem de folhas de estilo concebida inicialmente por Hampton Catlin e desenvolvida por Natalie Weizenbaum. Depois de suas versões iniciais, Weizenbaum e Chris Eppstein continuaram a estender Sass com SassScript, uma simples linguagem de script usada em arquivos Sass.',
     img: require('./assets/sass.gif')
   },
   {
-    Gear: 'BootStrap',
+    gear: 'BootStrap',
     ano: '2009',
     func: 'Bootstrap é um framework web com código-fonte aberto para desenvolvimento de componentes de interface e front-end para sites e aplicações web, usando HTML, CSS e JavaScript, baseado em modelos de design para a tipografia, melhorando a experiência do usuário em um site amigável e responsivo.',
-    img: require('./assets/bootstrap-stack.png')
+    img: require('./assets/bootstrap.gif')
   },
   {
-    Gear: 'Tailwind',
+    gear: 'Tailwind',
     ano: '2019',
     func: "Tailwind CSS é uma estrutura CSS de código aberto. A principal característica desta biblioteca é que, ao contrário de outros frameworks CSS como o Bootstrap, ela não fornece uma série de classes predefinidas para elementos como botões ou tabelas.",
-    img: require('./assets/wind.avif')
+    img: require('./assets/Tailwind.gif')
   },
 ];
